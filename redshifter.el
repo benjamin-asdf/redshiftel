384a06b83f18f018fa8b8163217266454ee4e0ad
Benj <Benjamin.Schwerdtner@gmail.com>
2019-07-10 20:16:04 +0200

    Add hello world code

---
+++ redshifter.el
@@ -0,0 +1,24 @@
+(defconst redshifter-default-screen-temp 5400)
+(defconst redshifter-step-size 100)
+
+(defvar redshifter-curr-screen-temp nil)
+
+(defun redshifter-set-screen-temp (temp)
+  (redshifter-reset-screen)
+  (shell-command (format "redshift -O %s" temp))
+  (setq redshifter-curr-screen-temp temp))
+
+(defun redshifter-reset-screen ()
+  (shell-command "redshift -x"))
+
+(defun redshifter-reset ()
+  (redshifter-reset-screen)
+  (setq redshifter-curr-screen-temp nil))
+
+(defun redshifter-increase ()
+  (interactive)
+  (let ((screen-temp))
+    (if redshifter-curr-screen-temp
+        (setq screen-temp (- redshifter-curr-screen-temp redshifter-step-size))
+      (setq screen-temp redshifter-default-screen-temp))
+    (redshifter-set-screen-temp screen-temp)))
